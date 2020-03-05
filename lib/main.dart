7ecc364ca10f4c739451218a141741c76a15e32e
BSISato <45438555+BSISato@users.noreply.github.com>
2020-03-05 00:58:49 -0300

    placar otera duas telas

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,13 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+int _contadorA = 0;
+int _contadorB = 0;
+String timeA = "TIME A";
+String timeB = "TIME B";
+
 class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
   AnimationController _controller;
-  int _contadorA = 0;
-  int _contadorB = 0;
 
   void _alteraContagemA(int comando) {
     setState(() {
@@ -23,6 +26,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
       if (_contadorA == -1) {
         //ORIENTADA A GAMBIARRA
         _contadorA = 0;
+      } else if (_contadorA >= 10 && _contadorB <= _contadorA - 2) {
+        showAlertDialogA(context);
+        _contadorA = 0;
+        _contadorB = 0;
       }
     });
   }
@@ -32,6 +39,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
       _contadorB += comando;
       if (_contadorB == -1) {
         //ORIENTADA A GAMBIARRA
+        _contadorB = 0;
+      } else if (_contadorB >= 10 && _contadorA <= _contadorB - 2) {
+        showAlertDialogB(context);
+        _contadorA = 0;
         _contadorB = 0;
       }
     });
@@ -50,7 +61,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.landscapeRight,
       DeviceOrientation.landscapeLeft,
-  ]);
+    ]);
     _controller = AnimationController(vsync: this);
   }
 
@@ -79,25 +90,30 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                 Padding(
                   padding: EdgeInsets.all(5.0),
                   child: Text(
-                    "Time A",
+                    "$timeA",
                     style: TextStyle(fontSize: 20.0, color: Colors.white),
                   ),
                 ),
                 Padding(
                   //ORIENTADA A GAMBIARRA
                   padding: EdgeInsets.all(5.0),
-                  child: FlatButton(
+                  child: RaisedButton(
                     child: Text(
-                      "",
+                      "RESULTADOS",
                       style: TextStyle(fontSize: 20.0, color: Colors.red),
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Segunda()),
+                      );
+                    },
                   ),
                 ),
                 Padding(
                   padding: EdgeInsets.all(5.0),
                   child: Text(
-                    "Time B",
+                    "$timeB",
                     style: TextStyle(fontSize: 20.0, color: Colors.white),
                   ),
                 ),
@@ -111,7 +127,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   padding: EdgeInsets.all(2.0),
                   child: Text(
                     "$_contadorA",
-                    style: TextStyle(fontSize: 140.0, color: Colors.white),
+                    style: TextStyle(fontSize: 120.0, color: Colors.white),
                   ),
                 ),
                 Padding(
@@ -129,7 +145,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   padding: EdgeInsets.all(2.0),
                   child: Text(
                     "$_contadorB",
-                    style: TextStyle(fontSize: 140.0, color: Colors.white),
+                    style: TextStyle(fontSize: 120.0, color: Colors.white),
                   ),
                 ),
               ],
@@ -140,10 +156,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
               children: <Widget>[
                 Padding(
                   padding: EdgeInsets.all(2.0),
-                  child: FloatingActionButton(
+                  child: FlatButton(
                     child: Text(
                       "+",
-                      style: TextStyle(fontSize: 30.0, color: Colors.white),
+                      style: TextStyle(fontSize: 55.0, color: Colors.black),
                     ),
                     onPressed: () {
                       _alteraContagemA(1);
@@ -163,10 +179,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                 ),
                 Padding(
                   padding: EdgeInsets.all(2.0),
-                  child: FloatingActionButton(
+                  child: FlatButton(
                     child: Text(
                       "+",
-                      style: TextStyle(fontSize: 30.0, color: Colors.white),
+                      style: TextStyle(fontSize: 55.0, color: Colors.black),
                     ),
                     onPressed: () {
                       _alteraContagemB(1);
@@ -181,10 +197,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
               children: <Widget>[
                 Padding(
                   padding: EdgeInsets.all(2.0),
-                  child: FloatingActionButton(
+                  child: FlatButton(
                     child: Text(
                       "-",
-                      style: TextStyle(fontSize: 30.0, color: Colors.white),
+                      style: TextStyle(fontSize: 55.0, color: Colors.black),
                     ),
                     onPressed: () {
                       _alteraContagemA(-1);
@@ -196,7 +212,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                   child: RaisedButton(
                     child: Text(
                       "ZERAR",
-                      style: TextStyle(fontSize: 20.0, color: Colors.red),
+                      style: TextStyle(fontSize: 25.0, color: Colors.red),
                     ),
                     onPressed: () {
                       _zera();
@@ -205,10 +221,10 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
                 ),
                 Padding(
                   padding: EdgeInsets.all(2.0),
-                  child: FloatingActionButton(
+                  child: FlatButton(
                     child: Text(
                       "-",
-                      style: TextStyle(fontSize: 30.0, color: Colors.white),
+                      style: TextStyle(fontSize: 55.0, color: Colors.black),
                     ),
                     onPressed: () {
                       _alteraContagemB(-1);
@@ -222,4 +238,137 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
       ],
     );
   }
+}
+/////////////////////////////////////////////////
+////////////////////////////////////////////////
+
+class Segunda extends StatefulWidget {
+  @override
+  _Segunda createState() => _Segunda();
+}
+
+class _Segunda extends State<Segunda> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          'images/placar.jpg',
+          fit: BoxFit.cover,
+          height: 1000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Text(
+                    "$timeA",
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: Text(
+                    "$timeB",
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Align(
+              //alignment: Alignment.bottomCenter,
+              child: FloatingActionButton(
+                child: Text(
+                  "<<<",
+                  style: TextStyle(fontSize: 20.0, color: Colors.white),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+showAlertDialogA(BuildContext context) {
+  // configura o button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+  // configura o  AlertDialog
+  AlertDialog alerta = AlertDialog(
+    title: Text("FIM DO JOGO"),
+    content: Text("Vitoria do $timeA\n\n"
+        "$timeA  $_contadorA  x  $_contadorB  $timeB"),
+    actions: [
+      okButton,
+    ],
+  );
+  // exibe o dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alerta;
+    },
+  );
+}
+
+showAlertDialogB(BuildContext context) {
+  // configura o button
+  Widget okButton = FlatButton(
+    child: Text("OK"),
+    onPressed: () {
+      Navigator.of(context).pop();
+    },
+  );
+  // configura o  AlertDialog
+  AlertDialog alerta = AlertDialog(
+    title: Text("FIM DO JOGO"),
+    content: Text("Vitoria do $timeB\n\n"
+        "$timeA  $_contadorA  x  $_contadorB  $timeB"),
+    actions: [
+      okButton,
+    ],
+  );
+  // exibe o dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alerta;
+    },
+  );
 }
